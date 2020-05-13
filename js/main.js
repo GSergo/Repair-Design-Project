2f2a9ade525f0cc2d5143a75a8c1b4055e350118
Сергей Губский <56925908+GSergo@users.noreply.github.com>
2020-05-13 20:51:15 +0700

    feat: add js function for modal(closing on key 'esc')

--- js/main.js
+++ js/main.js
@@ -4,20 +4,41 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal'); 
   //Записываем кнопку, вызывающую модальное окно, в переменную (отслеживаем по аттрибуту)
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+
   
-  const closeBtn = document.querySelector('.modal__close')
-  //Функция для вызова моадльного окна
+  //Функция для вызова модального окна
   const switchModal = () => {
-    //При клике присваиваем кнопке класс "modal--visible"
+    //При клике присваиваем класс "modal--visible"
     modal.classList.toggle('modal--visible');
   }
 
-  //Перебираем все кнопки, отслеживаем, был ли клик, выводим моадльное окно
+  //Перебираем все кнопки, отслеживаем, был ли клик, выводим модальное окно
   modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal)
+    element.addEventListener('click', switchModal);
 
   });
 
   //Закрываем мод.окно (toggle убирает класс visible)
-  closeBtn.addEventListener('click', switchModal)
+  closeBtn.addEventListener('click', switchModal);
+
+  //Убирает модальное окно при нажатии на Esc
+  document.addEventListener('keydown', function (esc) {
+    if (esc.keyCode === 27) {
+      modal.classList.remove('modal--visible');
+    };
+  });
+
+  //Убирает модальное окно при клике вне модального окна
+  //Пока не работает!
+  document.addEventListener('click', function (e) {
+
+    if (!body) {
+      modal.classList.remove('modal--visible');
+    };
+  });
+
+  console.log(modal.classList);
+
+
 });
